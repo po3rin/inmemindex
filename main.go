3bc76096a36fbd7c0319ea878de3a9139bb80335
中村 弘武 <abctail30@gmail.com>
2020-12-07 16:20:05 +0900

    add simple for debug

--- main.go
+++ main.go
@@ -3,53 +3,128 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
+	"log"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
-// docid index
-var postingsNode struct {
-	next *postingsNode
-	vals []int // doc id list
+const limit = 16
+
+var re = regexp.MustCompile("[^a-zA-Z 0-9]+")
+
+func clean(document string) string {
+	return re.ReplaceAllString(strings.ToLower(document), "")
 }
 
+func min(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
+
+// schema-independent index
+type postings struct {
+	ps   []int
+	next *postings
+}
+
+func (p *postings) put(pos int) {
+	if p.next != nil {
+		p.next.put(pos)
+		return
+	}
+
+	if cap(p.ps) == len(p.ps) {
+		p.next = &postings{
+			ps: make([]int, 0, min(limit, 8)),
+		}
+		p.next.put(pos)
+	}
+	p.ps = append(p.ps, pos)
+}
+
+func (p *postings) get() []int {
+	list := make([]int, 0)
+	return p.getTraverse(list)
+}
+
+func (p *postings) getTraverse(list []int) []int {
+	list = append(list, p.ps...)
+	if p.next != nil {
+		return p.next.getTraverse(list)
+	}
+	return list
+}
+
+type dict map[string]*postings
+
 type term struct {
-	id  int
-	val string
+	term string
+	pos  int
 }
 
 type terms []term
 
+// func (t terms) Len() int {
+// 	return len(t)
+// }
+
+// func (t terms) Swap(i, j int) {
+// 	t[i], t[j] = t[j], t[i]
+// }
+
+// func (t terms) Less(i, j int) bool {
+// 	return t[i].term < t[j].term
+// }
+
 func main() {
-	datadir := "testdata"
+	datadir := "simpledata"
+
 	files, err := ioutil.ReadDir(datadir)
 	if err != nil {
 		panic(err)
 	}
 
-	ht := NewHashTable()
-
-	for i, file := range files {
+	terms := make(terms, 0)
+	var pos int
+	for _, file := range files {
 		if file.IsDir() {
 			continue
 		}
 		b, err := ioutil.ReadFile(filepath.Join(datadir, file.Name()))
 		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+			log.Fatal(err)
 		}
-
 		tokens := strings.Split(string(b), " ")
 		for _, t := range tokens {
-			e, ok := ht.Get(t)
-			if !ok {
-
-			}
-			pn := postingsNode{
-				id:  i,
-				val: t,
-			}
+			pos++
+			t = clean(t)
+			terms = append(terms, term{
+				term: t,
+				pos:  pos,
+			})
 		}
+	}
+
+	dict := make(dict, 0)
+	for _, t := range terms {
+		var e *postings
+		e, ok := dict[t.term]
+		if !ok {
+			e = &postings{
+				ps: make([]int, 0, 4),
+			}
+			dict[t.term] = e
+		}
+		e.put(t.pos)
+	}
+
+	for t, e := range dict {
+		fmt.Println("--------------")
+		fmt.Printf("term: %v\n", t)
+		ps := e.get()
+		fmt.Printf("pos: %v\n", ps)
 	}
 }
